b96aed4b54986a84935f744d91f75cbe8827654f
izabelle-g <izabelle-g@users.noreply.github.com>
2022-12-09 23:40:38 -0700

    Finished browse/search page

--- js-files/browser-search.js
+++ js-files/browser-search.js
@@ -26,19 +26,7 @@ document.addEventListener("DOMContentLoaded", function(){
             const songList = JSON.parse(localStorage.getItem("songs"));
             
             /*--------------------------- Browse/Search Page -----------------------------*/
-            // populate select
-            const selectArtists = document.querySelector(".artists");
-            const selectGenres = document.querySelector(".genres");
-            populateSelect(artistList, selectArtists);
-            populateSelect(genreList, selectGenres);
-
-            // populate the table (no filter)
-            const tbody = document.querySelector(".tbl tbody");
-            displaySongs(songList, tbody);
-
-            // column sort
-            const btns = document.querySelectorAll('th a');
-            filter(songList, btns, tbody);
+            browseSearch(songList);
 
             /*--------------------------- Single Song Page-----------------------------*/
             const link = document.querySelectorAll(".title");
@@ -58,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function(){
             });
             //eventListener for the singleSong view
             let i = document.querySelectorAll(".songName");
-            console.log(i);
             i.forEach( (item) => {
                 item.addEventListener("click", (e) => {
                     console.log("yes");
@@ -77,19 +64,7 @@ document.addEventListener("DOMContentLoaded", function(){
         // retrieve song list
         const songList = JSON.parse(localStorage.getItem("songs"));
 
-        // populate select
-        const selectArtists = document.querySelector(".artists");
-        const selectGenres = document.querySelector(".genres");
-        populateSelect(artistList, selectArtists);
-        populateSelect(genreList, selectGenres);
-
-        // populate the table (no filter)
-        const tbody = document.querySelector(".tbl tbody");
-        displaySongs(songList, tbody);
-
-        // column sort
-        const btns = document.querySelectorAll('th a');
-        filter(songList, btns, tbody);
+        browseSearch(songList);
 
          /*--------------------------- Single Song Page-----------------------------*/
          const link = document.querySelectorAll(".title");
@@ -107,7 +82,6 @@ document.addEventListener("DOMContentLoaded", function(){
          let i = document.querySelectorAll(".songName");
          i.forEach( (item) => {
              item.addEventListener("click", (e) => {
-                 console.log("yes");
                  document.querySelector(".songInfo").style.display= "block";
                  document.querySelector(".search-browse").style.display = "none";
                  
@@ -171,7 +145,22 @@ function displayAnalysisData(details){
  * Browse/Search page functions
  */
 function browseSearch(songList){
-    
+    // populate select
+    const selectArtists = document.querySelector(".artists");
+    const selectGenres = document.querySelector(".genres");
+    populateSelect(artistList, selectArtists);
+    populateSelect(genreList, selectGenres);
+
+    const tbody = document.querySelector(".tbl tbody");
+
+    // populate the table (no filter)
+    displaySongs(songList, tbody);
+
+    // search song list
+    songSearch(songList, tbody);
+
+    // clear searched list
+    clearSearch(songList, tbody);
 }
 
 /**
@@ -219,12 +208,15 @@ function displaySongs(list, tbody){
         tr.appendChild(btn);
         tbody.appendChild(tr);
     });
+    filter(list, tbody);
 }
 
 /**
  * Filtering columns by sorting the columns
  */
-function filter(list, btns, tbody){
+function filter(list, tbody){
+    const btns = document.querySelectorAll('th a');
+
     for(let btn of btns){
         let sortField = btn.getAttribute("data-");
         let sorted;
@@ -245,11 +237,103 @@ function filter(list, btns, tbody){
                     break;
                 default:
                     sorted = list.sort( (a,b) => a[sortField] < b[sortField] ? -1:1);
+                    break;
             }
             displaySongs(sorted, tbody);
         }); 
     }
 }
+
+/**
+ * Search menu filtering
+ */
+function songSearch(list, tbody){
+    const radio = document.querySelectorAll(".input input");
+    const searchBtn = document.querySelector(".searchBtn");
+    const input = document.querySelector(".input");
+    const title = document.querySelector(".titleSearch");
+    const artist = document.querySelector(".artists");
+    const genre = document.querySelector(".genres");
+    let searchList;
+    let search;
+
+    // disable everything but the title
+    title.disabled = false;
+    artist.disabled = true;
+    genre.disabled = true;
+
+    input.addEventListener("change", () =>{
+    // diabled filters
+    radio.forEach( r => {
+        if(r.checked && r.title == "title"){
+            title.disabled = false;
+            artist.disabled = true;
+            genre.disabled = true;
+        }
+        if(r.checked && r.title == "artist"){
+            title.disabled = true;
+            artist.disabled = false;
+            genre.disabled = true;
+        }
+        if(r.checked && r.title == "genre"){
+            title.disabled = true;
+            artist.disabled = true;
+            genre.disabled = false;
+        }
+    });
+    });
+
+    
+
+    // check for checked radio button
+    searchBtn.addEventListener("click", () => {
+        let searchItem;
+        radio.forEach( r => {
+            if(r.checked){
+                search = r.title;
+            }
+        });
+
+        // filter song list
+        switch(search){
+            case "title":
+                searchItem = title.value;
+                searchList = list.filter(l => l.title.toLowerCase().includes(searchItem.toLowerCase()));
+                break;
+            case "artist":
+                // get artist id
+                searchItem = artist.value;
+                searchList = list.filter(l => l.artist.id == searchItem);
+                break;
+            case "genre":
+                // get genre id
+                searchItem = genre.value;
+                searchList = list.filter(l => l.genre.id == searchItem);
+                break;
+            default:
+                searchList = list;
+                break;
+        }
+        displaySongs(searchList, tbody);
+        filter(searchList, tbody);
+    });
+}
+
+/**
+ * Clears the search items and the results, and displays the whole list again
+ */
+function clearSearch(list, tbody){
+    const clearBtn = document.querySelector(".clearBtn");
+
+    clearBtn.addEventListener("click", () => {
+        const title = document.querySelector(".titleSearch").value = "";
+        const artist = document.querySelector(".artists").value = "";
+        const genre = document.querySelector(".genres").value = "";
+
+        displaySongs(list, tbody);
+    });
+}
+
 
 
 
